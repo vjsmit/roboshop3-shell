ccda989871162d7a789c80d7028298d5691de34f
vjsmit <vjsmit97@gmail.com>
2024-03-15 09:56:55 +0530

    rabbitmq setup

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,16 +1,16 @@
 echo -e "${}Setup Erlang repo${}"
-curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash     &>>${logfile}
 
 echo -e "${}Setup RabbitMQ repo${}"
-curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash    &>>${logfile}
 
 echo -e "${}Install Rabbitmq${}"
-dnf install rabbitmq-server -y
+dnf install rabbitmq-server -y    &>>${logfile}
 
 echo -e "${}Start RabbitMQ Service${}"
-systemctl enable rabbitmq-server
-systemctl restart rabbitmq-server
+systemctl enable rabbitmq-server    &>>${logfile}
+systemctl restart rabbitmq-server   &>>${logfile}
 
 echo -e "${}Create user for the app${}"
-rabbitmqctl add_user roboshop roboshop123
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+rabbitmqctl add_user roboshop roboshop123   &>>${logfile}
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"    &>>${logfile}
