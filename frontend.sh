c8a944ad9ee0cbae4e844288ae498a61214a7a5e
smvijay <vj.smit97@gmail.com>
2024-03-14 21:22:11 -0400

    cretaing frontend

---
+++ frontend.sh
@@ -0,0 +1,17 @@
+echo -e "\e[33mInstalling Nginx\e[0m"
+dnf install nginx -y
+
+echo -e "\e[33mRemove default content\e[0m"
+rm -rf /usr/share/nginx/html/*
+
+echo -e "\e[33mDownload and extract app content\e[0m"
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
+
+echo -e "\e[33mSetup Reverse proxy\e[0m"
+cp /home/centos/roboshop3-shell/frontend.conf /etc/nginx/default.d/roboshop.conf
+
+echo -e "\e[33mStart Nginx server\e[0m"
+systemctl enable nginx
+systemctl restart nginx
